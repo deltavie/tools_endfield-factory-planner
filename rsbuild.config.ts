1cdd6e1a6c27dba6c6fb3ef08ae8c5de57702edd
Eric <meaninglesslegacy@gmail.com>
2026-04-02 02:46:00 -0700

    update repo name

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@rsbuild/core';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   output: {
-    assetPrefix: '/deltavie-tools_endfield-factory-planner.github.io/',
+    assetPrefix: '/deltavie-tools_endfield-factory-planner/',
   },
 html: {
     title: 'Endfield Factory Planner'
