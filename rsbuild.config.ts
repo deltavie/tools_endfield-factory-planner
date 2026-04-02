21d36c7bb1990e8f05833048d90593289ce1e7c5
Eric <meaninglesslegacy@gmail.com>
2026-04-02 02:56:30 -0700

    deployment broken

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@rsbuild/core';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   output: {
-    assetPrefix: '/deltavie.github.io/tools_endfield-factory-planner/',
+    assetPrefix: '/tools_endfield-factory-planner/',
   },
 html: {
     title: 'Endfield Factory Planner'
